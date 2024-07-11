e16e1e2a470d9f23b583681b35ddc23a2c41f133
saranbabu09 <babusaran785@gmail.com>
2024-07-11 23:41:40 +0530

    praveen

--- src/Engineering/Engineering.java
+++ src/Engineering/Engineering.java
@@ -3,6 +3,8 @@ package Engineering;
 public class Engineering extends University {
 	public void eng() {
 		System.out.println("Engineering");
+		
+		System.out.println("Praveen is a Software Tester");
 	}
 
 }
